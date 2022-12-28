b21366239e6a9006f94b48dc03cfa49e4ce222b7
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2022-12-27 20:24:18 -0400

    refactor: deprecating code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "10xl": "9vw",
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
