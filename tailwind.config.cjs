aa6a767c5ba59a247c6fc522562da7d9a9f57567
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2022-12-29 12:35:17 -0400

    style: fonts + section component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["fira-code", ...defaultTheme.fontFamily.sans],
+      serif: ["lato", ...defaultTheme.fontFamily.serif],
+    },
     extend: {
       fontSize: {
         hidden: "0px",
