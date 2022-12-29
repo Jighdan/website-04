f0906aa086e4bb3064ae18fdb17a42b0674c4e23
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2022-12-29 17:34:36 -0400

    refactor: adding links for "Hero" section

---
+++ src/constants/links.ts
@@ -0,0 +1,7 @@
+export const LINKS = {
+	resume: '#',
+	email: 'mailto:reinny.almonte.ramos@gmail.com',
+	github: "https://github.com/jighdan",
+	linkedIn: 'https://www.linkedin.com/in/jighdan/',
+	codepen: 'https://codepen.io/jighdan'
+};
